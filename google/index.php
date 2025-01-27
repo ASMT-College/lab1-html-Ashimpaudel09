dda361748e0fdcd36971df1a0334160937c4da4d
Ashimpaudel09 <158179981+Ashimpaudel09@users.noreply.github.com>
2025-01-27 06:14:24 +0545

    Google

---
+++ google/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Google
+    </title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <form action="https://google.com/search" class="google" method="get" >
+        <h1 class="heading">
+            <img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_light_color_272x92dp.png" alt="">
+        </h1>
+        <form action="#">
+            <input type="search" name="q" placeholder="     Search Google or type a URL" id="isearch">
+        </form>
+       <div class="types" > <button>Google Search</button>
+        <button>I'm feeling lucky</button></div>
+        <p>Google offered in: <a href="">nepali</a></p>
+    </form>
+</body>
+</html>
